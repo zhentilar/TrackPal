0ac61fcdacac0399c5fa27572377aa88191be0d0
Zhentarim <54887323+zhentilar@users.noreply.github.com>
2024-11-03 20:55:34 +0300

    style css updated

--- app.py
+++ app.py
@@ -133,4 +133,4 @@ def update_target():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables if they don't exist
-    app.run(port=8000, debug=True)
+    app.run(port=5000, host="0.0.0.0")
